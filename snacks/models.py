1c5f1c0e464dc1dcdc07d3f1a0504fb88ea5c873
YahyaOmari <yahya.omari@outlook.com>
2021-06-15 21:59:35 +0300

    finished lab

--- snacks/models.py
+++ snacks/models.py
@@ -4,9 +4,9 @@ from django.urls import reverse
 # Create your models here.
 
 class Snack(models.Model):
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     description = models.TextField(max_length=256)
-    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
